60f98405409f29da954187391fa5d3bf074099e5
Danila Shpidonov <83277167+ShpidonovD@users.noreply.github.com>
2021-04-27 13:13:35 +0300

    Ex5

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/// author: Shpidonov Danila
+/// <summary>
+/// Написать программу обмена значениями двух переменных типа int без использования вспомогательных методов.
+/// а) с использованием третьей переменной;
+/// </summary> 
+
+
+using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"a = {a}  b = {b}");
+        }
+    }
+}
